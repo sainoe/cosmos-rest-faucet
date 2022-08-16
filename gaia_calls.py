4a4af032220361874eb11e2404a0d281ca8381fd
Simon <simon.ntz@gmail.com>
2022-08-16 16:20:32 +0200

    replace binary name in gaia.py

--- gaia_calls.py
+++ gaia_calls.py
@@ -1,5 +1,5 @@
 """
-gaiad utility functions
+interchain-security-pd utility functions
 - query bank balance
 - query tx
 - node status
@@ -13,9 +13,9 @@ import logging
 
 async def check_address(address: str, gaia_home: str = '~/.gaia'):
     """
-    gaiad keys parse <address>
+    interchain-security-pd keys parse <address>
     """
-    check = subprocess.run(["gaiad", "keys", "parse",
+    check = subprocess.run(["interchain-security-pd", "keys", "parse",
                             f"{address}",
                             f'--home={gaia_home}',
                             '--output=json'],
@@ -37,9 +37,9 @@ async def check_address(address: str, gaia_home: str = '~/.gaia'):
 
 async def get_balance_list(address: str, node: str, gaia_home: str = '~/.gaia'):
     """
-    gaiad query bank balances <address> <node> <chain-id>
+    interchain-security-pd query bank balances <address> <node> <chain-id>
     """
-    balance = subprocess.run(["gaiad", "query", "bank", "balances",
+    balance = subprocess.run(["interchain-security-pd", "query", "bank", "balances",
                               f"{address}",
                               f'--node={node}',
                               f'--home={gaia_home}',
@@ -69,12 +69,12 @@ async def tx_send(request: dict):
     - "fees"
     - "node"
     - "chain_id"
-    gaiad tx bank send <from address> <to address> <amount>
+    interchain-security-pd tx bank send <from address> <to address> <amount>
                        <fees> <node> <chain-id>
                        --keyring-backend=test -y
 
     """
-    tx_gaia = subprocess.run(['gaiad', 'tx', 'bank', 'send',
+    tx_gaia = subprocess.run(['interchain-security-pd', 'tx', 'bank', 'send',
                               f'{request["sender"]}',
                               f'{request["recipient"]}',
                               f'{request["amount"]}',
